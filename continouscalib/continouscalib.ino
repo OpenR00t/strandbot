b32af87fe5551e3cf31dc35db1233776e1837ba7
Open_root <rpclaird@gmail.com>
2016-05-17 10:07:27 -0400

    Calibration Sketch

---
+++ continouscalib/continouscalib.ino
@@ -0,0 +1,21 @@
+#include <Arduino.h>
+
+
+#include <Servo.h>
+
+Servo LeftServo; Servo RightServo;
+
+{
+  LeftServo.attach(9);
+  LeftServo.write(90);  // set servo speed
+
+  RightServo.attach(8);
+  RightServo.write(90);
+
+  pinMode(7, INPUT_PULLUP);// Enable internal pull-up resistor on pin 7 (xyjoy)
+   Serial.begin(9600);
+}
+
+void loop() {
+
+}
